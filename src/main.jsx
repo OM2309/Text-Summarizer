6ab0b365f4068186e83d106411457a02b5ebfffc
anurag <23anuragsharma@gmail.com>
2023-11-15 11:05:42 +0530

    Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./services/store";
 import App from "./App";
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+    <Provider store={store}>
       <App />
-    
+    </Provider>
   </React.StrictMode>
 );
